f4723bb3019e1d8161b384bab60dcdc9260e24ce
SALV <a01707122@tec.mx>
2024-05-09 19:11:15 -0600

    Arreglear formato Flake8

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,15 +1,4 @@
-"""
-Tic Tac Toe
-
-Exercises
-
-1. Give the X and O a different color and width.
-2. What happens when someone taps a taken spot?
-3. How would you detect when someone has won?
-4. How could you create a computer player?
-"""
-
-from turtle import *
+import turtle as tr
 from freegames import line
 
 
@@ -20,11 +9,14 @@ def grid():
     line(-200, -70, 200, -70)
     line(-200, 70, 200, 70)
 
+
 def actualizar_tablero(x, y, tablero):
     x_0 = int((x + 140) / 140)
     y_0 = int((y + 140) / 140)
     tablero[x_0][y_0] = 1
     return tablero
+
+
 def validar_tablero(x, y, tablero):
     x_0 = int((x + 140) / 140)
     y_0 = int((y + 140) / 140)
@@ -33,10 +25,11 @@ def validar_tablero(x, y, tablero):
     else:
         return False
 
+
 def drawx(x, y):
     # Dibujar pieza del jugador X
     size = 40
-    color("red")
+    tr.color("red")
     line(x - size, y - size, x + size, y + size)
     line(x - size, y + size, x + size, y - size)
 
@@ -44,12 +37,11 @@ def drawx(x, y):
 def drawo(x, y):
     # Dibujar pieza del jugador O
     radio = 50
-    up()
-    goto(x, y - radio)
-    down()
-    color("blue")
-    circle(radio)
-
+    tr.up()
+    tr.goto(x, y - radio)
+    tr.down()
+    tr.color("blue")
+    tr.circle(radio)
 
 
 def floor(value):
@@ -66,6 +58,7 @@ tablero = [
             [0, 0, 0]
             ]
 
+
 def tap(x, y):
     """Draw X or O in tapped square."""
     global tablero
@@ -75,19 +68,18 @@ def tap(x, y):
     player = state['player']
     draw = players[player]
 
-    if(validar_tablero(x, y, tablero)):
+    if (validar_tablero(x, y, tablero)):
         tablero = actualizar_tablero(x, y, tablero)
         draw(x, y)
-        update()
-
+        tr.update()
         # Intercambiar jugador
         state['player'] = not player
 
 
-setup(420, 420)
-hideturtle()
-tracer(False)
-grid()
-update()
-onscreenclick(tap)
-done()
+tr.setup(420, 420)
+tr.hideturtle()
+tr.tracer(False)
+tr.grid()
+tr.update()
+tr.onscreenclick(tap)
+tr.done()
